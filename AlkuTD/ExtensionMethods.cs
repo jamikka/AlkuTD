5c3c3b5ad53b2a7af464685d633a4a172b8da555
unknown <jkianto@gmail.com>
2019-09-24 15:14:02 +0300

    Win10-Soluutio-VS2019Comm-syys19

---
+++ AlkuTD/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlkuTD
+{
+    static class ExtensionMethods
+    {
+        public static string ZeroIfEmpty (this string s)
+        {
+            return string.IsNullOrEmpty(s) ? "0" : s;
+        }
+    }
+}
---
+++ AlkuTD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlkuTD
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new CurrentGame())
+                game.Run();
+        }
+    }
+}
